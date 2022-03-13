42132aaf366a7ac0c83d36d9e35142ce3ebdf14c
yash-03 <yksrivastava1@gmail.com>
2022-03-13 19:43:09 +0530

    react init

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Welcome to online car rental system
+      </header>
+    </div>
+  );
+}
+
+export default App;
